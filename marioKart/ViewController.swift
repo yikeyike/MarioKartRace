323525c2e3887641aef1d384df3dcf5efc3fa207
yike-penn <yikel@seas.upenn.edu>
2021-07-17 16:39:04 -0400

    Initial Commit

---
+++ marioKart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marioKart
+//
+//  Created by Yike Li on 7/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
